c105fd8e668335d9988fbb7d69d5495e1a78c8a3
Yeiduin <yoelyei22@gmail.com>
2022-09-08 19:05:16 -0500

    otro archivo

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3004, () => {
+  server.listen(process.env.PORT || 3004, () => {
     console.log("%s listening at 3004"); // eslint-disable-line no-console
   });
 });
